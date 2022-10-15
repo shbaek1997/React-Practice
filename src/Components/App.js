9fd3b1129e3ec889c0d9e943439886a52ac30529
shbaek1997 <shbaek1997@gmail.com>
2022-10-15 23:00:40 +0900

    Feat:added todo list

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,42 @@
+import { useState } from "react";
 import { Button } from "./Button";
-import styles from "../CSS/App.module.css";
+
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const onChange = (event) => {
+    setTodo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (todo !== "") {
+      setTodo("");
+      setTodos((currentArray) => {
+        const newArray = [...currentArray, todo];
+        return newArray;
+      });
+    }
+  };
   return (
     <div className="App">
-      <h1 className={styles.title}>Practicing React</h1>
-      <Button text="First Button" />
+      <h1>My Todo list: {todos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="todo_input">Type in your todo here</label>
+        <input
+          onChange={onChange}
+          type="text"
+          value={todo}
+          id="todo_input"
+        ></input>
+
+        {/* <button>Type in your todo</button> */}
+        <Button text="Submit your todo" />
+      </form>
+      <ul>
+        {todos.map((el, idx) => (
+          <li key={idx}>{el}</li>
+        ))}
+      </ul>
     </div>
   );
 }
